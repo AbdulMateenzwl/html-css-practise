ba905aedce966ad898cf154766f95119420bb6de
Abdul Mateen <98536971+notrealmateen@users.noreply.github.com>
2022-08-26 20:05:46 +0500

    tailwind css

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+//good for you i would say it is like to be the person of yours
+#include<iostream>
+using namespace std;
+main()
+{
+    // cout<<"Enter your name : ";
+    // string str;
+    // cin>>str;
+    // cout<<"Welcome "<<str;
+    for(int x=0;x<10000;x++)
+    {
+        cout<<x;
+    }
+}
